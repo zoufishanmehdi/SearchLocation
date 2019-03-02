b84db24f28874c4473eb2b20c54004529d3c372c
Zouf <zzmehdi@gmail.com>
2019-03-02 14:24:06 -0500

    SearchCitiesVC and MapDetailView added

---
+++ SearchLocation/Controllers/SearchCitiesVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchCitiesVC.swift
+//  SearchLocation
+//
+//  Created by Zoufishan Mehdi on 3/2/19.
+//  Copyright © 2019 Zoufishan Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class SearchCitiesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
