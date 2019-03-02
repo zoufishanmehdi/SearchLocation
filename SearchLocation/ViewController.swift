87462d2fc1a01f831e6c6722d501255428f6135e
Zouf <zzmehdi@gmail.com>
2019-03-02 13:01:23 -0500

    Initial Commit

---
+++ SearchLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchLocation
+//
+//  Created by Zoufishan Mehdi on 3/2/19.
+//  Copyright © 2019 Zoufishan Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SearchLocationTests/SearchLocationTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchLocationTests.swift
+//  SearchLocationTests
+//
+//  Created by Zoufishan Mehdi on 3/2/19.
+//  Copyright © 2019 Zoufishan Mehdi. All rights reserved.
+//
+
+import XCTest
+@testable import SearchLocation
+
+class SearchLocationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
