9863998cc43e0a79fac8c9665f6c3be804185d1c
Zouf <zzmehdi@gmail.com>
2019-03-02 16:30:40 -0500

    populating tableview with mock data

--- SearchLocation/Controllers/SearchCitiesVC.swift
+++ SearchLocation/Controllers/SearchCitiesVC.swift
@@ -8,23 +8,34 @@
 
 import UIKit
 
-class SearchCitiesVC: UIViewController {
+class SearchCitiesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cityViewModels = [CityViewModel]()
+    let cellId = "cityCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Search Locations"
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel!.text = "Alabama, US"
+        return cell
+    }
+    
 
 }
---
+++ SearchLocation/ViewModel/CityViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  CityViewModel.swift
+//  SearchLocation
+//
+//  Created by Zoufishan Mehdi on 3/2/19.
+//  Copyright © 2019 Zoufishan Mehdi. All rights reserved.
+//
+
+import Foundation
+
+struct CityViewModel {
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    
+    init(city: City) {
+        name = "\(city.name), \(city.country)"
+        latitude = city.coordinates.latitude
+        longitude = city.coordinates.longitude
+    }
+}
